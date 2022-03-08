3b017fa892dad38596f98c8d7ab2bc8755601dd6
Nguyen Hoan <96786536+5h4s1@users.noreply.github.com>
2022-03-08 23:34:02 +0700

    Update index.php

--- time_based/index.php
+++ time_based/index.php
@@ -1,19 +1,20 @@
 <?php
 require "config.php";
-if (isset($_GET['username']) && $_GET['password']) {
-    if (preg_match('/hex|substr|union|select|from/i', $_GET['username']))
+if (isset($_POST['username']) && $_POST['password']) {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+    if (preg_match('/hex|substr|union|select|from/i', $username))
         die("No hack");
 
-    if (preg_match('/hex|substr|union|select|from/i', $_GET['password']))
+    if (preg_match('/hex|substr|union|select|from/i', $password))
         die("No hack");
 
-    $username = "'" . $_GET['username'] . "'";
-    $password = "'" . $_GET['password'] . "'";
-    $query = "SELECT * FROM users  WHERE username = " . $username . " AND password = " . $password;
+    $query = "SELECT * FROM users  WHERE username = '$username' AND password = '$password'";
+    echo $query;
     $datas = $conn->query($query) ;
     if (isset($datas -> num_rows ) and $datas -> num_rows > 0) {
         $datas = $datas->fetch_assoc();
-        if ($datas['password'] === $_GET['password'] && $datas['username'] === $_GET['username']) {
+        if ($datas['password'] === $password && $datas['username'] === $username) {
             echo "Yes Yes Yes";
         } else {
             echo "No No No";
@@ -23,8 +24,21 @@ if (isset($_GET['username']) && $_GET['password']) {
         echo "No No No";
     }
     
-} else {
-    show_source(__FILE__);
-    die();
-}
+} 
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SQLi Time Based</title>
+</head>
+<body>
+    <form action="" method="post">
+        <input type="text" name="username"><br>
+        <input type="password" name="password"><br>
+        <input type="submit" name="submit" >
+    </form>
+</body>
+</html>
